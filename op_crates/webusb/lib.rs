a39f5ecc345157011dee6ffb6fef7f05ace315a7
Divy Srivastava <dj.srivastava23@gmail.com>
2021-02-17 07:36:04 +0000

    m-more

--- op_crates/webusb/lib.rs
+++ op_crates/webusb/lib.rs
@@ -8,6 +8,7 @@ use deno_core::serde_json::Value;
 use deno_core::JsRuntime;
 use deno_core::OpState;
 use deno_core::ZeroCopyBuf;
+use serde::Serialize;
 
 pub use rusb; // Re-export rusb
 
@@ -22,15 +23,36 @@ pub fn init(isolate: &mut JsRuntime) {
   }
 }
 
+#[derive(Serialize)]
 pub struct UsbConfiguration {
   // Index of String Descriptor describing this configuration.
   configuration_name: u8,
   // The configuration number. Should corresspond to bConfigurationValue (https://www.beyondlogic.org/usbnutshell/usb5.shtml#ConfigurationDescriptors)
   configuration_value: u8,
+  // TODO: implement USBInterfaces
 }
 
+#[derive(Serialize)]
 pub struct UsbDevice {
   configuration: UsbConfiguration,
+  // TODO: Implement configurations using https://docs.rs/rusb/0.7.0/rusb/struct.Device.html#method.config_descriptor
+  device_class: u8,
+  device_subclass: u8,
+  device_protocol: u8,
+  device_version_major: u8,
+  device_version_minor: u8,
+  device_version_subminor: u8,
+  // Need to open USB to get manufacturer_name https://docs.rs/rusb/0.7.0/rusb/struct.DeviceHandle.html#method.read_manufacturer_string_ascii
+  // manufacturer_name: String,
+  product_id: u16,
+  // Need to open USB to get product_name https://docs.rs/rusb/0.7.0/rusb/struct.DeviceDescriptor.html#method.product_string_index
+  // product_name: String,
+
+  // Need to open USB to get serial_number https://docs.rs/rusb/0.7.0/rusb/struct.DeviceDescriptor.html#method.serial_number_string_index
+  // serial_number: String,
+  usb_version_major: u8,
+  usb_version_minor: u8,
+  usb_version_subminor: u8,
 }
 
 pub fn op_webusb_get_devices(
@@ -43,13 +65,29 @@ pub fn op_webusb_get_devices(
 
   for device in devices.iter() {
     let config_descriptor = device.active_config_descriptor().unwrap();
+    let device_descriptor = device.device_descriptor().unwrap();
+    let device_version = device_descriptor.device_version();
+    let usb_version = device_descriptor.usb_version();
+
     let configuration = UsbConfiguration {
       configuration_name: config_descriptor.description_string_index().unwrap(),
       configuration_value: config_descriptor.number(),
     };
 
-    usbdevices.push(UsbDevice { configuration });
+    usbdevices.push(UsbDevice {
+      configuration,
+      device_class: device_descriptor.class_code(),
+      device_subclass: device_descriptor.sub_class_code(),
+      device_protocol: device_descriptor.protocol_code(),
+      device_version_major: device_version.major(),
+      device_version_minor: device_version.minor(),
+      device_version_subminor: device_version.sub_minor(),
+      product_id: device_descriptor.product_id(),
+      usb_version_major: usb_version.major(),
+      usb_version_minor: usb_version.minor(),
+      usb_version_subminor: usb_version.sub_minor(),
+    });
   }
 
-  Ok(json!({}))
+  Ok(json!(usbdevices))
 }
